177517070a8ab93d7260fbaa02798fef7b91834c
saguhwi <saguhwiyono999@gmail.com>
2019-07-21 20:36:27 -0700

    Pertemuan12b

---
+++ Pertemuan12b/app/src/main/java/com/example/pertemuan12b/konfigurasi.java
@@ -0,0 +1,23 @@
+package com.example.pertemuan12b;
+
+public class konfigurasi {
+
+    public static final String URL_ADD = "https://pegawai.terimajasa.com/tambahpgw.php";
+    public static final String URL_GET_ALL = "https://pegawai.terimajasa.com/tampilsemuapgw.php";
+    public static final String URL_GET_EMP = "https://pegawai.terimajasa.com/tampilpgw.php?id=";
+    public static final String URL_UPDATE_EMP = "https://pegawai.terimajasa.com/updatepgw.php";
+    public static final String URL_DELETE_EMP = "https://pegawai.terimajasa.com/hapuspgw.php?id=";
+
+    public static final String KEY_EMP_ID = "id";
+    public static final String KEY_EMP_NAMA = "name";
+    public static final String KEY_EMP_POSISI = "position";
+    public static final String KEY_EMP_GAJIH = "salary";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "name";
+    public static final String TAG_POSISI = "position";
+    public static final String TAG_GAJIH = "salary";
+
+    public static final String EMP_ID = "emp_id";
+}
